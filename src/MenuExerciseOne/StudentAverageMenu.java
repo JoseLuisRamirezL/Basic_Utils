c5a189ca1b4624797b9306735fd5bd16fa06400b
JoseLuisRamirezL <jramirezlarios@deloitte.com>
2024-08-07 16:28:43 -0600

    Exercise 1 Complete :)

--- src/MenuExerciseOne/StudentAverageMenu.java
+++ src/MenuExerciseOne/StudentAverageMenu.java
@@ -1,43 +1,83 @@
 package MenuExerciseOne;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class StudentAverageMenu{
     private final Scanner scanner = new Scanner(System.in);
     private String name;
     private int signatures;
-    private int grade;
+    private String grade;
+    private String[] signaturesNames = new String[signatures];
+    private double[] signaturesGrades = new double[signatures];
 
     public void startMenuStudentAverage() {
-        System.out.println("Please enter your name:");
-        name = scanner.nextLine();
+        try {
 
-        System.out.println("Please enter your grade:");
-        grade = scanner.nextInt();
+            System.out.println("Please enter your name:");
+            name = scanner.nextLine();
 
-        System.out.println("Please enter the number of signatures to enroll:");
-        signatures = scanner.nextInt();
-        scanner.nextLine();
+            System.out.println("Please enter your grade:");
+            grade = scanner.next();
 
-        String[] signaturesNames = IntStream.range(0, signatures)
-                .mapToObj(i -> {
-                    System.out.println("Enter the name of the signature " + (i + 1) + ": ");
-                    return scanner.nextLine();
-                })
-                .toArray(String[]::new);
+            System.out.println("Please enter the number of signatures to enroll:");
+            signatures = scanner.nextInt();
+            scanner.nextLine();
 
-        int[] signaturesGrades = IntStream.range(0, signatures)
-                        .map(i -> {
-                            System.out.println("Enter the grade of the signature " + signaturesNames[i] + ": ");
-                            return scanner.nextInt();
-                        })
-                        .toArray();
+            signaturesGrades = new double[signatures];
+            signaturesNames = new String[signatures];
 
-        System.out.println(Arrays.toString(signaturesNames));
-        System.out.println(Arrays.toString(signaturesGrades));
+            IntStream.range(0, signatures)
+                    .forEach(i -> {
+                        System.out.println("Enter the name of the signature " + (i + 1) + ":");
+                        signaturesNames[i] = scanner.nextLine();
+                        System.out.println("Enter the score for " + signaturesNames[i] + ":");
+                        signaturesGrades[i] = scanner.nextDouble();
+                        scanner.nextLine();
+                    });
+            printOutput();
+        } catch (Exception e) {
+            System.out.println("An error has occurred" + e.getMessage());
+            System.out.println("Back to the Main Menu...");
+        }
     }
+
+    private void printOutput(){
+        System.out.println();
+        System.out.println();
+        System.out.println(createLine());
+        String format = "Student name: %-40s Grade: %s%n";
+        String format2 = "    Signature: %-39s Score: %.1f%n";
+        System.out.printf(format, name, grade);
+        System.out.println(createLine());
+        IntStream.range(0, signatures)
+                .forEach(i -> {
+                    System.out.printf(format2, signaturesNames[i], signaturesGrades[i]);
+                });
+        System.out.println(createLine());
+        if (signatureAverage() <= 5 && signatureAverage() >= 0 ){
+            System.out.printf("Final average: %.2f             Status: Not passed" ,signatureAverage());
+        }
+        else if (signatureAverage() >= 6 && signatureAverage() <= 7 ){
+            System.out.printf("Final average: %.2f             Status: Passed with regular level" ,signatureAverage());
+        } else if (signatureAverage() >= 8 && signatureAverage() <=9) {
+            System.out.printf("Final average: %.2f             Status: Passed with good level" ,signatureAverage());
+        } else if (signatureAverage() > 9 && signatureAverage() <= 10 ) {
+            System.out.printf("Final average: %.2f             Status: Passed with perfect level", signatureAverage());
+        } else {
+            System.out.println("Error, you didn't enter values from 1 to 10, back to the main menu...");
+        }
+    }
+
+    private double signatureAverage(){
+        return Arrays.stream(signaturesGrades).average().getAsDouble();
+    }
+
+    private String createLine(){
+        return IntStream.range(0, 70)
+                .mapToObj(i -> "-")
+                .collect(Collectors.joining());
+    }
+
 }
